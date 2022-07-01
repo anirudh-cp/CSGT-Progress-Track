b963d11c9bc2a38b658a97e5bf8fde0fb63ca016
Kritin D Madhavan <100946679+KritinDMadhavan@users.noreply.github.com>
2022-07-01 10:21:38 +0530

    update serializers.py
    We created a serializers.py file and imported all models from Models.py. By using the serializers we can easily connect to Rest framework

--- backend/backend/api/serializers.py
+++ backend/backend/api/serializers.py
@@ -1,7 +1,37 @@
 from rest_framework import serializers
-from .models import SampleModel
+from .models import *
 
-class SampleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SampleModel
-        fields = ["text",  "user"]
+class personalserializer(serializers.ModelSerializer):
+	class Meta:
+		model=personal
+		fields='__all__'
+
+class conferneceserializer(serializers.ModelSerializer):
+	class Meta:
+		model=conference
+		fields='__all__'
+
+class journalserializer(serializers.ModelSerializer):
+	class Meta:
+		model=journal
+		fields='__all__'
+
+class bookchapterserializer(serializers.ModelSerializer):
+	class Meta:
+		model=book_chapters
+		fields='__all__'
+
+class bookeditorserializer(serializers.ModelSerializer):
+	class Meta:
+		model=book_editor
+		fields='__all__'
+
+class consultancyserializer(serializers.ModelSerializer):
+	class Meta:
+		model=consultancy_project
+		fields='__all__'
+
+class patentserializer(serializers.ModelSerializer):
+	class Meta:
+		model=patent
+		fields='__all__'
