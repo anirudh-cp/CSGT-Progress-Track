92de143b1de7d036c57e4210f1749d2a7a54815a
Anirudh <cp.anirudh@outlook.com>
2022-07-05 00:46:25 +0530

    Publications Endpoint + OpenAPI + Models
    Publications end point has been implemented. It can handle changes to all 4 models.
    Date convertor has been added to parse date passed to the API.
    Models have been updated:
        - The personal model now has a primary key of EmpID, It returns the Emp_ID when called as an object.
        - The conference model has had the name field removed as Emp_ID is sufficient.
        - The DOI, Emp_ID and author position together form a unique field constraint.
    The OpenAPI docs have been updated to better reflect the use of the GET request.

---
+++ backend/backend/api/Views/Publications.py
@@ -0,0 +1,93 @@
+from email.policy import HTTP
+from requests import delete
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+
+import sys
+
+from uritemplate import partial
+sys.path.append('../..')
+
+from api.models import conference, journal, book_chapters, book_editor
+from api.serializers import conferneceserializer, journalserializer, bookchapterserializer, bookeditorserializer
+
+model = {'conference': conference, 'journal':journal, 'book_chapters': book_chapters, 'book_editor': book_editor}
+serilaizerTypes = {'conference': conferneceserializer, 'journal':journalserializer, 
+                   'book_chapters': bookchapterserializer, 'book_editor': bookeditorserializer}
+
+
+class PublicationsSingleGetApiView(APIView):
+    # add permission to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    # 1. List single
+    def get(self, request, Emp_ID, Type, startDate, endDate, *args, **kwargs):
+        ''' List text for given requested user. '''
+        
+        queryData = (model[Type].objects.filter(Emp_ID=Emp_ID, 
+                                               Conference_startdate__gt=startDate, 
+                                               Conference_startdate__lte=endDate) | 
+                     model[Type].objects.filter(Emp_ID=Emp_ID, 
+                                               Conference_startdate__lte=startDate, 
+                                               Conference_enddate__gte=startDate))
+        
+        
+        if queryData.exists():
+            serializer = serilaizerTypes[Type](queryData, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+            
+        return Response("", status=status.HTTP_404_NOT_FOUND)
+
+
+
+class PublicationsSinglePutApiView(APIView):
+    # add permission to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+
+    # 2. Create/Update
+    def put(self, request, Emp_ID, Type, *args, **kwargs):
+        ''' Create/Update the record with given data. '''
+        
+        data = request.data
+        queryData = model[Type].objects.filter(Emp_ID=Emp_ID, DOI=data['DOI'])
+        if queryData.exists():
+            data.pop('Type', None)
+            serializer = serilaizerTypes[Type](queryData[0], data=data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            serializer = serilaizerTypes[Type](data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class PublicationsAllApiView(APIView):
+    # add permission to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    # 1. List all
+    def get(self, request, Type, startDate, endDate, *args, **kwargs):
+        ''' Get all records. '''
+        
+        queryData = (model[Type].objects.filter(Conference_startdate__gt=startDate, 
+                                                Conference_startdate__lte=endDate) | 
+                     model[Type].objects.filter(Conference_startdate__lte=startDate, 
+                                                Conference_enddate__gte=startDate))
+        
+        
+        if queryData.exists():
+            serializer = serilaizerTypes[Type](queryData, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+            
+        return Response("", status=status.HTTP_404_NOT_FOUND)
+
+        
--- backend/backend/api/urls.py
+++ backend/backend/api/urls.py
@@ -1,11 +1,17 @@
-
-from django.urls import path, include
+from django.urls import path, include, register_converter
 
 from .Views.Faculty import FacultySingleApiView, FacultyAllApiView
+from .Views.Publications import PublicationsSingleGetApiView, PublicationsSinglePutApiView, PublicationsAllApiView
+from .convertors import DateConverter
 
+register_converter(DateConverter, 'date')
 
 urlpatterns = [
     path('faculty/<int:Emp_ID>', FacultySingleApiView.as_view()),
     path('faculty', FacultyAllApiView.as_view()),
+    
+    path('publications/<int:Emp_ID>/<Type>/<date:startDate>/<date:endDate>', PublicationsSingleGetApiView.as_view()),
+    path('publications/<int:Emp_ID>/<Type>', PublicationsSinglePutApiView.as_view()),
+    path('publications/<Type>/<date:startDate>/<date:endDate>', PublicationsAllApiView.as_view()),
 ]
 
