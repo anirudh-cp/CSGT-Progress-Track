af8315b2a2581364e9892c5527a8ded942747005
Anirudh <cp.anirudh@outlook.com>
2022-08-12 16:33:13 +0530

    Change base dir

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,7 @@ import dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
