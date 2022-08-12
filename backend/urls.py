582cdee864d04c2187ea2091fe95aaf916d6d8a1
Anirudh <cp.anirudh@outlook.com>
2022-08-12 20:39:08 +0530

    Added template view path

--- backend/urls.py
+++ backend/urls.py
@@ -19,8 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 from api import urls as api_urls
 
+from django.views.generic import TemplateView
+from django.urls import re_path
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(api_urls)),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
